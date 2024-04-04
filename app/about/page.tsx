45434664d6f259421aedd0c62484e34a68bbef99
taylor <taylorgig265@gmail.com>
2024-04-04 19:44:53 +0200

    update3

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between w-full my-16 lg:py-8">
+        <h1>About-Page</h1>
+    </main>
+    
+  )
+}
--- app/{components}/Navbar.tsx
+++ app/{components}/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
@@ -9,32 +10,28 @@ const Navbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
         <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
+          <Link href={'/products'}>Products</Link>
         </li>
-        <li><a>Item 3</a></li>
+        <li>
+        <Link href={'/about'}>About-Us</Link>
+          
+        </li>
+        <li><Link href={'/contact'}>Contact-Us</Link></li>
       </ul>
     </div>
     <a className="btn btn-ghost text-xl">daisyUI</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+    <li>
+          <Link href={'/products'}>Products</Link>
+        </li>
+        <li>
+        <Link href={'/about'}>About-Us</Link>
+          
+        </li>
+        <li><Link href={'/contact'}>Contact-Us</Link></li>
     </ul>
   </div>
   <div className="navbar-end">
